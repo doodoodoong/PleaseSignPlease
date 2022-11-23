9d48392f88b08528d45269535b301a9d0e146e63
doodoodoong <doo_doong@hanmail.net>
2022-11-23 23:32:16 +0900

    first

---
+++ PleaseSign/layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-app-bar absolute color="#02343F" elevate-on-scroll dense>
+      <v-btn fab height="45px" width="45px" icon>
+        <v-icon>mdi-home-heart</v-icon>
+      </v-btn>
+      <v-tolbar-title>Please Sign Please</v-tolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-information-outline</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed">
+      <span>Copyright 2022. doodoong all rights reserved.</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {}
+  },
+}
+</script>
---
+++ PleaseSign/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{js,veu,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
