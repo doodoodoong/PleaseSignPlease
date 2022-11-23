185a6b39fd6a84114dbc08b502751902e7627c69
doodoodoong <doo_doong@hanmail.net>
2022-11-23 23:43:51 +0900

    first

--- PleaseSign/layouts/default.vue
+++ PleaseSign/layouts/default.vue
@@ -1,10 +1,10 @@
 <template>
   <v-app>
-    <v-app-bar absolute color="#02343F" elevate-on-scroll dense>
+    <v-app-bar app absolute color="#02343F" elevate-on-scroll dense>
       <v-btn fab height="45px" width="45px" icon>
         <v-icon>mdi-home-heart</v-icon>
       </v-btn>
-      <v-tolbar-title>Please Sign Please</v-tolbar-title>
+      <v-tolbar-title class="text-yellow-50">Please Sign Please</v-tolbar-title>
       <v-spacer></v-spacer>
       <v-btn icon>
         <v-icon>mdi-information-outline</v-icon>
@@ -15,8 +15,10 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-footer :absolute="!fixed">
-      <span>Copyright 2022. doodoong all rights reserved.</span>
+    <v-footer :absolute="!fixed" color="#02343F">
+      <span class="text-yellow-50"
+        >Copyright 2022. doodoong all rights reserved.</span
+      >
     </v-footer>
   </v-app>
 </template>
